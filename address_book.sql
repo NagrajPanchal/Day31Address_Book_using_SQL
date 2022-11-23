655d5c213236e997bd6d885b51d7ac7f3c582c02
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-24 02:31:30 +0530

    Added : Create table Address book

--- address_book.sql
+++ address_book.sql
@@ -1,3 +1,16 @@
 create database address_book;
 show databases;
 use address_book;
+
+/* create Table Address Book */
+create table address_book (
+first_name varchar(30),
+last_name varchar(30),
+address varchar(40),
+city varchar(30),
+state varchar(30) default 'Karnataka', 
+zip int, 
+phone_number long not null,
+email varchar(50),
+primary key(first_name)
+);
